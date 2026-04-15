ea2df5996332dc8fa934a6463361008af695841d
Sekhudin <sekhudinuap@gmail.com>
2026-04-16 04:30:33 +0700

    feat: new error builder

--- internal/errors/error.go
+++ internal/errors/error.go
@@ -9,6 +9,14 @@ type ErrorCode string
 const (
 	ErrConfigLoad     ErrorCode = "CONFIG_LOAD_FAILED"
 	ErrConfigNotFound ErrorCode = "CONFIG_NOT_FOUND"
+
+	ErrFlagRead   ErrorCode = "FLAG_READ_FAILED"
+	ErrValidation ErrorCode = "VALIDATION_FAILED"
+	ErrIO         ErrorCode = "IO_OPERATION_FAILED"
+	ErrTemplate   ErrorCode = "TEMPLATE_RENDER_FAILED"
+	ErrRuntime    ErrorCode = "RUNTIME_EXECUTION_FAILED"
+	ErrDependency ErrorCode = "DEPENDENCY_FAILED"
+	ErrInternal   ErrorCode = "INTERNAL_ERROR"
 )
 
 type CoreError struct {
@@ -54,5 +62,67 @@ func NewConfigNotFoundError(scope string, msg string) error {
 		Code:    ErrConfigNotFound,
 		Scope:   scope,
 		Message: msg,
+	}
+}
+
+func NewFlagReadError(flagName string, err error) error {
+	return &CoreError{
+		Code:    ErrFlagRead,
+		Scope:   flagName,
+		Message: "failed to read flag value",
+		Err:     err,
+	}
+}
+
+func NewValidationError(scope, msg string) error {
+	return &CoreError{
+		Code:    ErrValidation,
+		Scope:   scope,
+		Message: msg,
+	}
+}
+
+func NewIOError(scope, msg string, err error) error {
+	return &CoreError{
+		Code:    ErrIO,
+		Scope:   scope,
+		Message: msg,
+		Err:     err,
+	}
+}
+
+func NewTemplateError(scope, msg string, err error) error {
+	return &CoreError{
+		Code:    ErrTemplate,
+		Scope:   scope,
+		Message: msg,
+		Err:     err,
+	}
+}
+
+func NewRuntimeError(scope, msg string, err error) error {
+	return &CoreError{
+		Code:    ErrRuntime,
+		Scope:   scope,
+		Message: msg,
+		Err:     err,
+	}
+}
+
+func NewDependencyError(scope, msg string, err error) error {
+	return &CoreError{
+		Code:    ErrDependency,
+		Scope:   scope,
+		Message: msg,
+		Err:     err,
+	}
+}
+
+func NewInternalError(scope, msg string, err error) error {
+	return &CoreError{
+		Code:    ErrInternal,
+		Scope:   scope,
+		Message: msg,
+		Err:     err,
 	}
 }
