2ed3e25a24da42e578ad0dd5394d77d56984bfde
Sekhudin <sekhudinuap@gmail.com>
2026-04-27 02:43:24 +0700

    chore: add recover function

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/sekhudin/trax/cmd"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Fprintln(os.Stderr, "[BUG]", r)
+			os.Exit(1)
+		}
+	}()
+
 	cmd.Execute()
 }
